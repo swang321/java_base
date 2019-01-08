52ff6ab6fbbef16da91c5c15c8d95fff0813f5e0
swang321 <978530201@qq.com>
2019-01-08 22:37:44 +0800

    0.0

---
+++ src/main/java/search/BinarySearch.java
@@ -0,0 +1,41 @@
+package search;
+
+/**
+ * 必须是线性结构  必须是有序的
+ *
+ * @author swang
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] array = {5, 13, 19, 21, 37, 56, 64, 75, 80, 88, 92};
+        int key = 92;
+        int index = binarySearch(array, key);
+
+        System.out.println(index);
+
+    }
+
+    private static int binarySearch(int[] array, int key) {
+        int low = 0;
+        int height = array.length - 1;
+        return binarySearch(array, key, low, height);
+    }
+
+    private static int binarySearch(int[] array, int key, int low, int height) {
+
+        if (low > height) {
+            return -1;
+        }
+        int mid = (low + height) / 2;
+        if (key == array[mid]) {
+            return mid;
+        } else if (key < array[mid]) {
+            return binarySearch(array, key, low, height - 1);
+        } else {
+            return binarySearch(array, key, low + 1, height);
+        }
+
+    }
+}
