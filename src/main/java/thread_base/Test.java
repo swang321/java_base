2096e4ace701833b22e5f4a0bd7e0fc2942c66e9
swang321 <978530201@qq.com>
2019-04-09 11:14:00 +0800

    多线程

--- src/main/java/thread_base/Test.java
+++ src/main/java/thread_base/Test.java
@@ -1,10 +1,7 @@
 package thread_base;
 
-import javax.xml.transform.Result;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -18,25 +15,8 @@ public class Test {
 
         Map map = new HashMap(16);
 
-        
-
 
     }
-
-    void solve(Executor e, Collection<Callable<Result>> solvers) throws InterruptedException, ExecutionException {
-        CompletionService<Result> ecs = new ExecutorCompletionService<Result>(e);
-        for (Callable<Result> s : solvers) {
-            ecs.submit(s);
-        }
-        int n = solvers.size();
-        for (int i = 0; i < n; ++i) {
-            Result r = ecs.take().get();
-            if (r != null) {
-                System.out.println(r);
-            }
-        }
-    }
-
 
 }
 
